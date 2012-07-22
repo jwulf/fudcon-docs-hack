eeb3394c3b4bf40907d052cf058348febc75b9f6
Sitapati <sitapati@worldsankirtan.net>
2012-07-22 16:30:10 +1000

    Code languages are now alpha-sorted, and the variablelist role is the more semantic 'codetabs'

--- source/publican-fedora-video/en-US/css/code-lang-switcher.js
+++ source/publican-fedora-video/en-US/css/code-lang-switcher.js
@@ -1,4 +1,16 @@
+
+
 $($(function(){
+
+function addNewListItem($list, $ele) {
+    $list.append($ele);
+    var listItems = $list.children('li').get();
+    listItems.sort(function(a, b) {
+       return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+    });
+    $.each(listItems, function(idx, itm) { $list.append(itm); });
+}
+
 var language_css_codes = {
     'JavaScript':'js',
     'Node.js' :'node',
@@ -9,7 +21,7 @@ var language_css_codes = {
     'Java': 'java',
     'html': 'HTML'
   };
-  $('div.code-div').each(function() {
+  $('div.codetabs').each(function() {
     var div_code = $(this);
     var counter = 0;
     var ul = $(document.createElement('ul'));
@@ -17,10 +29,8 @@ var language_css_codes = {
       lang = $(this).children('.term')[0].innerHTML
       var linkEl = $('<a href="#" rel="'+language_css_codes[lang]+'">'+lang+'</a>');
       var listItemEl = $('<li class="code-select-li"></li>');
-      // Right now it's adding them in the order the author put them
-      // Probably should sort alphabetically
+      // Alpha sort idea from here: http://stackoverflow.com/questions/11509555/insert-new-list-sorted-in-alphabetical-order-javascript-or-jquery
       listItemEl.append(linkEl);
-      ul.append(listItemEl);
       proglisting = $(this).next();
       proglisting.addClass('code-lang-' + language_css_codes[lang]);
       
@@ -36,11 +46,14 @@ var language_css_codes = {
         div_code.find('a').removeClass('active');
         $(this).addClass('active');
       });
-      
-      // This should store the lang pref (perhaps in a cookie) and show that by default on page load
-      div_code.prepend(ul)
-      div_code.find('dd:not(:first)').hide();
-      div_code.find('a:first').addClass('active');
+  
+            addNewListItem(ul, listItemEl);
+            // This should store the lang pref (perhaps in a cookie) and show that by default on page load
     });
+      div_code.prepend(ul)
+      initialtab = div_code.find('a:first');
+      initialtab.addClass('active');
+      div_code.find('dd').hide();
+      div_code.find('.code-lang-'+ initialtab.attr('rel')).show();
   });
 }))
